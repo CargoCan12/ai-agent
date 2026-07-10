d47a16ecae40666dd7e0deb3785a621194429f6f
CargoCan12 <cargocan@pm.me>
2026-07-09 23:23:06 -0600

    added calculator

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
-from vars import messages, model
-from client import client
+import os
+import argparse
+from dotenv import load_dotenv
+from openai import OpenAI
+#from vars import messages, model, args
+#from client import client
+
+
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+model = 'openrouter/free'
+
+messages = [
+    {
+        "role": "user", "content": args.user_prompt
+    }
+]
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+if api_key is None:
+    raise RuntimeError("API Key didn't load")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
 
 
 def main():
     print("Hello from ai!")
-    response = client.chat.completions.create(model = model, messages=messages)
-    if response.usage != None:
-        print(f"User prompt: {messages[0]['content']}")
+    response = client.chat.completions.create(model="openrouter/free", messages=messages,)
+    if not response.usage:
+        raise RuntimeError("API response appears to be malformed")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage.prompt_tokens}")
         print(f"Response tokens: {response.usage.completion_tokens}")
-        print(response.choices[0].message.content)
-    else:
-        raise RuntimeError("Did not complete as dialed")
+    print(response.choices[0].message.content)
+
 
 if __name__ == "__main__":
     main()
--- vars.py
+++ vars.py
@@ -1,13 +1,15 @@
 import argparse
-model = 'openrouter/free'
-messages=[
-    {
-        "role": "user",
-        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    }
-]
+
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
 # Now we can access `args.user_prompt`
+
+model = 'openrouter/free'
+
+messages = [
+    {
+        "role": "user", "content": args.user_prompt
+    }
+]
